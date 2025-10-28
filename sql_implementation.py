f97f3df6c4ee2ac6ccd5fb9dd03c96de803a7fc8
Vivian Hernandex <vah7365@rit.edu>
2025-10-27 23:54:45 -0400

    Changes: load data 3/4 - 1 issue with transactions

--- sql_implementation.py
+++ sql_implementation.py
@@ -29,32 +29,46 @@ def loadData(conn):
     """
     Read in the data
     """
-
-    #Account files 
-    account_files = {
-        "banking_datasets/baseline/baseline_accounts.csv",
-        "banking_datasets/edgecases/edgecases_accounts.csv",
-        "banking_datasets/hotspot/hotspot_accounts.csv",
-        "banking_datasets/payday/payday_accounts.csv",
+   
+    #CUSTOMER file
+    customer_files = {
+        "banking_datasets/baseline/baseline_customers.csv",
+        "banking_datasets/edgecases/edgecases_customers.csv",
+        "banking_datasets/hotspot/hotspot_customers.csv",
+        "banking_datasets/payday/payday_customers.csv",
     }
 
-    # for file in account_files: 
-    #     read = pd.read_csv(file,skiprows=1,usecols=[0,1,2,3,4,5,6,7])
+    with conn.cursor() as cur:
+        for file in customer_files: 
+            read = pd.read_csv(file,skiprows=1,usecols=[0,1,2,3,4])
+            for row in read.itertuples(index=False): 
+                customer_id = row[0]
+                name = row[1]
+                email = row[2]
+                phone_number = row[3]
+                creation = row[4]
 
- 
-    # file = pd.read_csv(TrafficFile,skiprows=1, usecols=[0,1])
-    # for row in file.itertuples(index=False): 
+                sql = """
+                        INSERT INTO customer (customer_id,name, email, phone_number, creation_date )
+                        VALUES (%s, %s, %s,%s,%s)
+                        ON CONFLICT (customer_id) DO NOTHING;
+                    """
+                cur.execute(sql, (customer_id,name,email,phone_number,creation))
         
-   
-    #customer files 
+        conn.commit()
+        print("customer data loaded")
 
-    #read in the merchant files and load them into the db 
+
+
+
+    #MERCHANT - read in the merchant files and load them into the db 
     merchant_files = {
         "banking_datasets/baseline/baseline_merchants.csv",
         "banking_datasets/edgecases/edgecases_merchants.csv",
         "banking_datasets/hotspot/hotspot_merchants.csv",
         "banking_datasets/payday/payday_merchants.csv",
     }
+
     with conn.cursor() as cur:
         for file in merchant_files: 
             read = pd.read_csv(file,skiprows=1,usecols=[0,1,2])
@@ -73,8 +87,71 @@ def loadData(conn):
         conn.commit()
         print("Merchant data loaded")
 
-            
-    #transaction files
+
+    # #Account files 
+    account_files = {
+        "banking_datasets/baseline/baseline_accounts.csv",
+        "banking_datasets/edgecases/edgecases_accounts.csv",
+        "banking_datasets/hotspot/hotspot_accounts.csv",
+        "banking_datasets/payday/payday_accounts.csv",
+    }
+
+    with conn.cursor() as cur:
+        for file in account_files: 
+            read = pd.read_csv(file,skiprows=1,usecols=[0,1,2,3,4,5,6])
+            for row in read.itertuples(index=False): 
+                account_id = row[0]
+                customer_id= row[1]
+                balance = row[2]
+                overdraft = row[3]
+                status = row[4]
+                open_at = row[5]
+                update = row[6]
+
+                sql = """
+                        INSERT INTO account (account_id, customer_id, open_date,status,balance, last_update,overdraft_limit)
+                        VALUES (%s, %s, %s,%s,%s,%s,%s)
+                        ON CONFLICT (account_id) DO NOTHING;
+                    """
+                cur.execute(sql, (account_id,customer_id,open_at,status,balance,update,overdraft))
+        
+        conn.commit()
+        print("account data loaded")
+
+
+
+    #transaction files -> minor issue - 10001 not present 
+
+    transaction_files = {
+        "banking_datasets/baseline/baseline_transactions.csv",
+        "banking_datasets/edgecases/edgecases_transactions.csv",
+        "banking_datasets/hotspot/hotspot_transactions.csv",
+        "banking_datasets/payday/payday_transactions.csv",
+    }
+
+    with conn.cursor() as cur:
+        for file in transaction_files: 
+            read = pd.read_csv(file,skiprows=1,usecols=[0,1,2,3,4,5,6,7,8])
+            for row in read.itertuples(index=False): 
+                transaction_id = row[0]
+                account_id = row[1]
+                time_stamp = row[2]
+                amount = row[3]
+                type = row[4]
+                transfer_id = int(row[5]) if not pd.isna(row[5]) else None
+                channel = row[6] if not pd.isna(row[6]) else None
+                merchant_id = int(row[7]) if not pd.isna(row[7]) else None
+                note = row[8] if not pd.isna(row[8]) else None
+                
+                sql = """
+                        INSERT INTO transactions (transaction_id, account_id, merchant_id,type, time_stamp, amount,channel, note, transfer_id)
+                        VALUES (%s, %s, %s,%s,%s,%s,%s, %s, %s)
+                        ON CONFLICT (transaction_id) DO NOTHING;
+                    """
+                cur.execute(sql, (transaction_id, account_id, merchant_id,type, time_stamp, amount,channel, note, transfer_id))
+        
+        conn.commit()
+        print("transaction data loaded")
 
 
 
@@ -86,7 +163,7 @@ def loadData(conn):
 
 ############## BANK FUNCTIONS ############## 
 
-def OpenAccount():
+def OpenAccount(conn):
     """
     create a new account for a customer with status open and balance 0.
     """
